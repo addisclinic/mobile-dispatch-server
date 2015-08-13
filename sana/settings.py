166d31b7945e65e226962da33b741272310e0f83
Michael Romano <michaelfromano@gmail.com>
2015-08-13 17:02:25 -0400

    undid settings.py changes

--- sana/settings.py
+++ sana/settings.py
@@ -26,34 +26,29 @@ TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
     # ('Your Name', 'your_email@domain.com'),
-    ('Mike', 'michaelfromano@gmail.com'),
 )
 ''' Tuple of admin names and email addresses. '''
 
 MANAGERS = ADMINS
 
 ### Database settings
-if DEBUG:
-    DATABASE_ENGINE = 'sqlite3'
-    DATABASE_NAME = 'devdb'
-else:
-    DATABASE_ENGINE = os.environ['DATABASE_ENGINE'] or ''
-    """'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'."""
+DATABASE_ENGINE = os.environ['DATABASE_ENGINE'] or ''
+"""'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'."""
 
-    DATABASE_NAME = ''
-    """Or path to database file if using sqlite3."""
+DATABASE_NAME = ''
+"""Or path to database file if using sqlite3."""
 
-    DATABASE_USER = ''
-    """Not used with sqlite3."""
+DATABASE_USER = ''
+"""Not used with sqlite3."""
 
-    DATABASE_PASSWORD = ''
-    """Not used with sqlite3."""
+DATABASE_PASSWORD = ''
+"""Not used with sqlite3."""
 
-    DATABASE_HOST = ''
-    """Set to empty string for localhost. Not used with sqlite3."""
+DATABASE_HOST = ''
+"""Set to empty string for localhost. Not used with sqlite3."""
 
-    DATABASE_PORT = ''
-    """Set to empty string for default. Not used with sqlite3."""
+DATABASE_PORT = ''
+"""Set to empty string for default. Not used with sqlite3."""
 
 TIME_ZONE = 'America/Chicago'
 """Local time zone for this installation. Choices can be found here:
